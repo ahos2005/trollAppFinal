42ab7831fba074ac76fa9adbec03a7a2e84629cb
Ahmed Elhosseiny <aelhosse@ucsd.edu>
2014-06-07 10:01:34 -0700

    Initial Commit

---
+++ Troll/src/edu/ucsd/troll/app/SplashScreen.java
@@ -0,0 +1,35 @@
+package edu.ucsd.troll.app;
+
+
+ 
+public class SplashScreen extends Activity {
+ 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 2500;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+ 
+        new Handler().postDelayed(new Runnable() {
+ 
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+ 
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+ 
+}
